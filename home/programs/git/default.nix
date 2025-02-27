a05b8706afb87777bc74e0286e75d37a5ec41e03
Lucas Elisei <lucas@elisei.ch>
2025-02-27 10:29:06 +0100

    Update git configuration

--- home/programs/git/default.nix
+++ home/programs/git/default.nix
@@ -1,17 +1,43 @@
-# { vars, ... }:
-{
+{pkgs, ...}: {
   programs.git = {
     enable = true;
 
     extraConfig = {
+      branch.sort = "-committerdate";
+      column.ui = "auto";
+      commit.verbose = true;
+      credential.helper = "${pkgs.git.override {withLibsecret = true;}}/bin/git-credential-libsecret";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      fetch = {
+        all = true;
+        prune = true;
+        pruneTags = true;
+      };
       init.defaultBranch = "main";
+      merge.conflictStyle = "zdiff3";
       pull.rebase = true;
-      push.autoSetupRemote = true;
+      push = {
+        autoSetupRemote = true;
+        default = "simple";
+        followTags = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      rerere = {
+        autoUpdate = true;
+        enabled = true;
+      };
+      tag.sort = "version:refname";
     };
 
     userName = "Lucas Elisei";
-    # userEmail = if vars.isWork
-    # 	then "lelisei@tandemdiabetes.com"
-    # 	else "lucas@elisei.ch";
   };
 }
