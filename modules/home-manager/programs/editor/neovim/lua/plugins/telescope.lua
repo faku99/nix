3166543181f4a0ee8a673f126decfa0b25b8a3db
Lucas Elisei <lucas@elisei.ch>
2025-06-26 15:30:25 +0200

    Update nvim configuration

--- modules/home-manager/programs/editor/neovim/lua/plugins/telescope.lua
+++ modules/home-manager/programs/editor/neovim/lua/plugins/telescope.lua
@@ -1,11 +1,10 @@
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<leader>ff', builtin.find_files)
-
 return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim'
+  },
+  keys = {
+    { '<leader>ff', '<cmd>Telescope find_files<cr>' }
   }
 }
