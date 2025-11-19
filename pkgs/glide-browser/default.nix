efedde81cfd4f37977ea84c4a4cacbafc9e7f98a
Lucas Elisei <lucas@elisei.ch>
2025-11-19 14:29:59 +0100

    Fix glide-browser package

--- pkgs/glide-browser/default.nix
+++ pkgs/glide-browser/default.nix
@@ -1,51 +1,111 @@
 {
-  pkgs,
   stdenv,
-  fetchurl,
-  makeWrapper,
-  lib,
-}: let
-  buildInputs = [
-    stdenv.cc.cc.lib
-    pkgs.gcc.libc
-    pkgs.glibc
-    pkgs.libgcc
+  pkgs,
+  ...
+}:
+stdenv.mkDerivation rec {
+  pname = "glide-browser";
+  version = "0.1.54a";
+  src = let
+    sources = {
+      "x86_64-linux" = pkgs.fetchurl {
+        url = "https://github.com/glide-browser/glide/releases/download/${version}/glide.linux-x86_64.tar.xz";
+        sha256 = "sha256-Rw85b+9eaiM9szWpYZiF7FqJY7OpliOwt09/c8UWlGk=";
+      };
+    };
+  in
+    sources.${pkgs.system};
+  nativeBuildInputs = with pkgs; [
+    wrapGAppsHook3
+    autoPatchelfHook
+    patchelfUnstable
   ];
-  libraryPath = lib.makeLibraryPath buildInputs;
-
-  version = "0.1.53a";
-in
-  stdenv.mkDerivation {
-    pname = "glide-browser";
-    inherit version;
-
-    src = fetchurl {
-      url = "https://github.com/glide-browser/glide/releases/download/${version}/glide.linux-x86_64.tar.xz";
-      sha256 = "sha256-tU+o6YKtlOX9rDQBS8OJOv3NNlk/mM14keIHDVPO5Ec=";
-    };
-
-    nativeBuildInputs = [
-      makeWrapper
+  buildInputs = with pkgs; [
+    gtk3
+    adwaita-icon-theme
+    gdk-pixbuf
+    cairo
+    pango
+    atk
+    glib
+    libcanberra-gtk3
+    xorg.libX11
+    xorg.libxcb
+    xorg.libXScrnSaver
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXi
+    xorg.libXrandr
+    xorg.libXtst
+    xorg.libxshmfence
+    xorg.libXxf86dga
+    xorg.libXxf86vm
+    xorg.libXt
+    libdrm
+    libGL
+    mesa
+    libglvnd
+    vulkan-loader
+    alsa-lib
+    pipewire
+    ffmpeg
+    libfido2
+    libu2f-host
+    libusb-compat-0_1
+    opensc
+    pam_u2f
+    yubico-pam
+    pcsc-tools
+    dbus-glib
+    cups
+    stdenv.cc
+    zlib
+    speechd-minimal
+    libkrb5
+    desktop-file-utils
+  ];
+  runtimeDependencies = with pkgs;
+    [
+      curl
+      pciutils
+      libva.out
+      libnotify
+      udev
+      libgbm
+    ]
+    ++ buildInputs;
+  appendRunpaths = with pkgs; [
+    "${pipewire}/lib"
+    "${libglvnd}/lib"
+    "${mesa}/lib"
+  ];
+  patchelfFlags = ["--no-clobber-old-sections"];
+  sourceRoot = ".";
+  installPhase = ''
+    mkdir -p $out/lib/glide-browser-${version}
+    cp -r glide/* $out/lib/glide-browser-${version}/
+    mkdir -p $out/bin
+    ln -s $out/lib/glide-browser-${version}/glide $out/bin/glide
+    ln -s $out/bin/glide $out/bin/glide-browser
+  '';
+  preFixup = ''
+    gappsWrapperArgs+=(
+      --set MOZ_LEGACY_PROFILES 1
+      --set MOZ_ALLOW_DOWNGRADE 1
+      --set-default MOZ_ENABLE_WAYLAND 1
+      --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath runtimeDependencies}"
+    )
+  '';
+  meta = {
+    description = "Glide Browser";
+    homepage = "https://github.com/glide-browser/glide";
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
     ];
-
-    buildInputs = buildInputs;
-
-    dontBuild = true;
-
-    installPhase = ''
-      mkdir -p $out/bin
-      mkdir -p $out/lib/glide
-      tar xf $src --directory=$out/lib/glide --strip-components=1
-      chmod +x $out/lib/glide/glide-bin
-
-      makeWrapper $out/lib/glide/glide-bin $out/bin/glide \
-        --set LD_LIBRARY_PATH "${libraryPath}"
-    '';
-
-    meta = with lib; {
-      description = "Glide Browser";
-      homepage = "https://glide-browser.app/";
-      license = licenses.unfree;
-      platforms = ["x86_64-linux"];
-    };
-  }
+    mainProgram = "glide";
+  };
+}
