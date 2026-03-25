501e17d1df4667960e8eaad7a420816785c24614
Lucas Elisei <lucas@elisei.ch>
2026-03-25 09:02:58 +0100

    Add defaultBrowser option for glide-browser

--- modules/home-manager/programs/browser/glide-browser/default.nix
+++ modules/home-manager/programs/browser/glide-browser/default.nix
@@ -10,12 +10,24 @@ in
 {
   options.userConfig.programs.browser.glide-browser = {
     enable = lib.mkEnableOption "glide-browser";
+
+    defaultBrowser = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Set glide-browser as default browser";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
       glide-browser
     ];
+
+    userConfig.programs.browser = lib.mkIf cfg.defaultBrowser {
+      enable = true;
+      executable = "librewolf";
+      desktop = "librewolf.desktop";
+    };
 
     xdg.desktopEntries = {
       glide-browser = {
