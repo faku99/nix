2c9c834fc8553f49e167d315af497db91bd953bf
Lucas Elisei <lucas@elisei.ch>
2026-04-21 14:21:43 +0200

    Delete prospect-mail

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
   curseforge = pkgs.callPackage ./curseforge {};
-  prospect-mail = pkgs.callPackage ./prospect-mail {};
   wallpaper-gen = pkgs.callPackage ./wallpaper-gen {};
 }
