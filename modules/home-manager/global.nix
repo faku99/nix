8ff8786f8335aea364b5b9644f388ca12ca06439
Lucas Elisei <lucas@elisei.ch>
2026-04-27 17:19:35 +0200

    feat(global): update Nix trusted caches

--- modules/home-manager/global.nix
+++ modules/home-manager/global.nix
@@ -21,15 +21,25 @@ in
     nix = {
       package = mkDefault pkgs.nix;
       settings = {
+        auto-optimise-store = true;
         experimental-features = [
           "flakes"
           "nix-command"
         ];
-        substituters = ["https://hyprland.cachix.org"];
-        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-        trusted-substituters = ["https://hyprland.cachix.org"];
+
+        substituters = [
+          "https://cache.nixos.org"
+          "https://nix-community.cachix.org"
+          "https://hyprland.cachix.org"
+        ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        ];
         trusted-users = [
           "lelisei"
+          "root"
         ];
       };
     };
