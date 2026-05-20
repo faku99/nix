406a76f40ad8ba1c55ac88c669281c94b2c01038
Lucas Elisei <lucas@elisei.ch>
2026-05-20 10:58:02 +0200

    refactor(substituters): move extra-substituters to substituters

--- modules/home-manager/global.nix
+++ modules/home-manager/global.nix
@@ -26,19 +26,17 @@ in
           "flakes"
           "nix-command"
         ];
-        extra-substituters = [ "https://noctalia.cachix.org" ];
-        extra-trusted-public-keys = [
-          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-        ];
         substituters = [
           "https://cache.nixos.org"
-          "https://nix-community.cachix.org"
           "https://hyprland.cachix.org"
+          "https://nix-community.cachix.org"
+          "https://noctalia.cachix.org"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         ];
         trusted-users = [
           "lelisei"
