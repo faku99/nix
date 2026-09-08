ec929481b3ad531c862ca818569b70aa70ab218f
Lucas Elisei <lucas@elisei.ch>
2026-09-08 15:23:28 +0200

    chore(noctalia): bump to v5.0.1

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia/refs/tags/v5.0.0-beta.9";
+      url = "github:noctalia-dev/noctalia/refs/tags/v5.0.1";
     };
     stylix = {
       url = "github:nix-community/stylix";
