7dd4f77b5cc2bc7c416da36214ab27d66c97e1e3
Lucas Elisei <lucas@elisei.ch>
2026-09-15 08:52:31 +0200

    feat(saturn): update configuration

--- modules/hosts/saturn/default.nix
+++ modules/hosts/saturn/default.nix
@@ -89,8 +89,7 @@
         den.aspects.oh-my-opencode-slim
 
         den.aspects.brave
-        den.aspects.zen
-        den.aspects.librewolf-default
+        den.aspects.zen-default
 
         den.aspects.direnv
         den.aspects.git
