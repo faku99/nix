fa67413479bc81bca7d38784d225a4aad794be3b
Lucas Elisei <lucas@elisei.ch>
2026-09-16 10:04:49 +0200

    feat(zen): disable stylix theming

--- modules/programs/browser/zen.nix
+++ modules/programs/browser/zen.nix
@@ -1,10 +1,18 @@
 { den, inputs, ... }:
 {
-  den.aspects.zen.homeManager = {
-    imports = [ inputs.zen-browser.homeModules.default ];
+  den.aspects.zen.homeManager =
+    {
+      imports = [ inputs.zen-browser.homeModules.default ];
 
-    programs.zen-browser.enable = true;
-  };
+      programs.zen-browser.enable = true;
+
+      stylix.targets.zen-browser = {
+        # Disable stylix for Zen
+        enable = false;
+        # Silence warning
+        profileNames = [ "default" ];
+      };
+    };
 
   den.aspects.zen-default = {
     includes = [ den.aspects.zen ];
